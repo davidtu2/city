ff60067eca57efa51b85a85b4ee930dffe10d86f
davidtu2 <david.tu2@csu.fullerton.edu>
2017-10-07 10:59:04 -0700

    Added a Texture class

--- hello_city.cpp
+++ hello_city.cpp
@@ -196,6 +196,76 @@ class Camera{
 		}
 };
 
+class Texture{
+	public:
+		Texture(){//Note the use of multiple Texture constructor signatures serving different purposes
+			_faces = {"textures/right.tga", "textures/left.tga", "textures/top.tga", "textures/bottom.tga", "textures/back.tga", "textures/front.tga"};
+
+    		glGenTextures(1, &_texture);//Create 1 texture that are of type unsigned int, as indicated by the texture type
+    		glBindTexture(GL_TEXTURE_CUBE_MAP, _texture);//Bind the texture so that any texture commands called after apply to this texture
+    		for (unsigned int i = 0; i < _faces.size(); i++){
+        		_data = stbi_load(_faces[i].c_str(), &_width, &_height, &_colorChannels, 0);
+        		if (_data){//Adding by i because OpenGL's enums is linearly incremented. It will go through:
+					//GL_TEXTURE_CUBE_MAP_POSITIVE_X, GL_TEXTURE_CUBE_MAP_NEGATIVE_X, GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
+					//GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, GL_TEXTURE_CUBE_MAP_POSITIVE_Z and GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
+            		glTexImage2D(	GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,//Generate the texture by using the uploaded data. This needs to be done 6 times for each face
+									0,//Specifies the mipmap level
+									GL_RGB,//How to store the texture: rgba
+									_width, _height, 
+									0,//Specifies border size
+									GL_RGB,//The format of the data: rgb
+									GL_UNSIGNED_BYTE,//The datatype of the data
+									_data);
+					stbi_image_free(_data);
+        		}else{
+					printf("Cubemap texture failed to load.\n");
+					stbi_image_free(_data);
+					exit(1);
+        		}
+    		}//Configure the texture with texture settings:
+    		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);//Bi-linear filtering is used to clean up any minor aliasing when the camera rotates.
+    		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			//Texture coordinates that are exactly between two faces might not hit an exact face (due to some hardware limitations)
+    		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);//so by using GL_CLAMP_TO_EDGE, OpenGL always return their edge values whenever we sample between faces.
+    		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);//Specify how to wrap each texture coordinate
+    		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);//If you don't clamp to edge then you might get a visible seam on the edges of your textures
+		}
+
+		Texture(std::string path){//Generate a texture for all of the buildings
+			//Note that we can generate a texture for each building object but that will require more resources. In fact when tried, it works but the program will be very slow
+			glGenTextures(1, &_texture);
+			glBindTexture(GL_TEXTURE_2D, _texture);
+			_data = stbi_load(path.c_str(), &_width, &_height, &_colorChannels, 0);
+        	if (_data){
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGB, GL_UNSIGNED_BYTE, _data);
+				stbi_image_free(_data);
+       		}else{
+				printf("Building texture failed to load.\n");
+				stbi_image_free(_data);
+				exit(1);
+       		}
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glBindTexture(GL_TEXTURE_2D, 0);
+		}
+
+		virtual ~Texture(){}
+
+		unsigned int getTexture(){
+			return _texture;
+		}
+
+	private:
+		std::vector<std::string> _faces;//The inner faces of the skybox
+		unsigned int _texture;//Skybox texture
+		unsigned char* _data;//Texture data
+		int _width;//Texture's width and height
+		int _height;
+		int _colorChannels;//Corresponds to a texture's rgba
+};
+
 class Building{
 	public:
 		Building(float x, float y, float z, float size, float height, unsigned int texture):_x(x), _y(y), _z(z), _size(size), _height(height), _texture(texture), _noWindowsPerRow(1){}
@@ -273,41 +343,9 @@ class Building{
 
 class Plane{
 	public:
-		Plane(int size):_size(size), _block(10.0f){//Generate a texture for all of the buildings
-			//Note that we can generate a texture for each building object but that will require more resources. In fact when tried, it works but the program will be very slow
-			glGenTextures(1, &_texture);
-			glBindTexture(GL_TEXTURE_2D, _texture);
-			_data = stbi_load("textures/building.jpg", &_width, &_height, &_colorChannels, 0);
-        	if (_data){
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGB, GL_UNSIGNED_BYTE, _data);
-				stbi_image_free(_data);
-       		}else{
-				printf("Building texture failed to load.\n");
-				stbi_image_free(_data);
-				exit(1);
-       		}
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-			glBindTexture(GL_TEXTURE_2D, 0);
-
-			glGenTextures(1, &_texture1);
-			glBindTexture(GL_TEXTURE_2D, _texture1);
-			_data = stbi_load("textures/building2.jpg", &_width, &_height, &_colorChannels, 0);
-        	if (_data){
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGB, GL_UNSIGNED_BYTE, _data);
-				stbi_image_free(_data);
-       		}else{
-				printf("Building texture failed to load.\n");
-				stbi_image_free(_data);
-				exit(1);
-       		}
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-			glBindTexture(GL_TEXTURE_2D, 0);
+		Plane(int size):_size(size), _block(10.0f){
+			_building1 = new Texture("textures/building.jpg");
+			_building2 = new Texture("textures/building2.jpg");
 
 			//Proceduaral generation reference: https://bitbucket.org/whleucka/cpsc-graphics-final/src/856ef81f67cf92f90c84368965331069d2de4e0f/src/main.cpp?at=master&fileviewer=file-view-default
 			for(int j = -2; j > -_size - 6; j -= 6){
@@ -322,9 +360,9 @@ class Plane{
 						}
 						int randomTexture = rand() % 2;
 						if(randomTexture == 0){
-							_building = new Building(i, 0.0f, j, randomSize, randomHeight, _texture);
+							_building = new Building(i, 0.0f, j, randomSize, randomHeight, _building1->getTexture());
 						}else{
-							_building = new Building(i, 0.0f, j, randomSize, randomHeight, _texture1);
+							_building = new Building(i, 0.0f, j, randomSize, randomHeight, _building2->getTexture());
 						}
 						printf("The random texture is %d.\n", randomTexture);
 						_buildings.push_back(_building);
@@ -373,12 +411,8 @@ class Plane{
 		float _block;//Size of the block (a.k.a. the length of the "street")
 		std::vector<Building*> _buildings;
 		Building* _building;//Building to be inserted into XZ's vector
-		unsigned int _texture;
-		unsigned int _texture1;
-		unsigned char* _data;//Texture data
-		int _width;//Texture's width and height
-		int _height;
-		int _colorChannels;//Corresponds to texture's rgba
+		Texture* _building1;
+		Texture* _building2;
 };
 
 class World{
@@ -439,36 +473,8 @@ class World{
 				GL_FALSE,//Is the data normalized?
 				3 * sizeof(float),//How much data per row
 				(void*)0);//How much data I need to skip over
-			_faces = {"textures/right.tga", "textures/left.tga", "textures/top.tga", "textures/bottom.tga", "textures/back.tga", "textures/front.tga"};
 
-    		glGenTextures(1, &_texture);//Create 1 texture that are of type unsigned int, as indicated by the texture type
-    		glBindTexture(GL_TEXTURE_CUBE_MAP, _texture);//Bind the texture so that any texture commands called after apply to this texture
-    		for (unsigned int i = 0; i < _faces.size(); i++){
-        		_data = stbi_load(_faces[i].c_str(), &_width, &_height, &_colorChannels, 0);
-        		if (_data){//Adding by i because OpenGL's enums is linearly incremented. It will go through:
-					//GL_TEXTURE_CUBE_MAP_POSITIVE_X, GL_TEXTURE_CUBE_MAP_NEGATIVE_X, GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
-					//GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, GL_TEXTURE_CUBE_MAP_POSITIVE_Z and GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
-            		glTexImage2D(	GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,//Generate the texture by using the uploaded data. This needs to be done 6 times for each face
-									0,//Specifies the mipmap level
-									GL_RGB,//How to store the texture: rgba
-									_width, _height, 
-									0,//Specifies border size
-									GL_RGB,//The format of the data: rgb
-									GL_UNSIGNED_BYTE,//The datatype of the data
-									_data);
-					stbi_image_free(_data);
-        		}else{
-					printf("Cubemap texture failed to load.\n");
-					stbi_image_free(_data);
-					exit(1);
-        		}
-    		}//Configure the texture with texture settings:
-    		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);//Bi-linear filtering is used to clean up any minor aliasing when the camera rotates.
-    		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			//Texture coordinates that are exactly between two faces might not hit an exact face (due to some hardware limitations)
-    		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);//so by using GL_CLAMP_TO_EDGE, OpenGL always return their edge values whenever we sample between faces.
-    		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);//Specify how to wrap each texture coordinate
-    		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);//If you don't clamp to edge then you might get a visible seam on the edges of your textures
+			_skybox = new Texture();
 		}
         
 		virtual ~World(){
@@ -486,7 +492,7 @@ class World{
         	glBindVertexArray(_VAO);//skybox cube
 			glActiveTexture(GL_TEXTURE0);//Activate the texture unit first before binding. This allows us to use multiple textures. If this is not called, the default will be: GL_TEXTURE0
 			//bind the texture before drawing to the texture unit specified earlier. This also makes it available in the fragment shader as a sampler uniform
-        	glBindTexture(GL_TEXTURE_CUBE_MAP, _texture);
+        	glBindTexture(GL_TEXTURE_CUBE_MAP, _skybox->getTexture());
         	glDrawArrays(GL_TRIANGLES, 0, 36);
         	glBindVertexArray(0);
         	glDepthFunc(GL_LESS);//set depth function back to default
@@ -497,12 +503,7 @@ class World{
 		Plane* _XZ;//the XZ plane
 		unsigned int _VAO;//The following private variables are for the skybox
 		unsigned int _VBO;
-		std::vector<std::string> _faces;//The inner faces of the skybox
-		unsigned int _texture;//Skybox texture
-		unsigned char* _data;//Texture data
-		int _width;//Texture's width and height
-		int _height;
-		int _colorChannels;//Corresponds to a texture's rgba
+		Texture* _skybox;
 };
 
 class HelloGLSLApp : public GLFWApp{
